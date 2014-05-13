9d327afa43e2027d8fa12348399a4d29b7d62d8d
akomar <ak.komar@gmail.com>
2014-05-12 22:20:57 -0400

    Refactoring first problem.

--- code/spark/lesson3/lesson3.sbt
+++ code/spark/lesson3/lesson3.sbt
@@ -9,3 +9,7 @@ libraryDependencies += "org.apache.spark" %% "spark-core" % "0.9.1"
 resolvers += "Akka repository" at "http://repo.akka.io/releases"
 
 libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "1.2.1"
+
+//running:
+//sbt package
+//sbt run
--- code/spark/lesson3/src/main/scala/Lesson3.scala
+++ code/spark/lesson3/src/main/scala/Lesson3.scala
@@ -1,20 +1,28 @@
-/*** SimpleApp.scala ***/
+/** * SimpleApp.scala ***/
+
+import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
 object Lesson3 {
   def main(args: Array[String]) {
-    val purchasesFile = "/home/training/udacity_training/data/purchases.txt" 
+    val purchasesFile = "/home/training/udacity_training/data/purchases.txt"
     val sc = new SparkContext("local", "Purchases", "/home/training/spark-0.9.1-bin-hadoop1/",
       List("target/scala-2.10/lesson-3_2.10-1.0.jar"))
+
     val purchasesData = sc.textFile(purchasesFile).sample(false, 0.01, 1)
-    val toysAndElectronicsSales = purchasesData.map(line => line.split("\t"))
-                                               .filter(purchase => purchase(3)=="Toys" 
-                                                                || purchase(3)=="Consumer Electronics")
-                                               .map(purchase => (purchase(3),scala.math.BigDecimal(purchase(4))))
-                                               .reduceByKey((a,b)=>a+b)
-                 
+
+    val toysAndElectronicsSales = computeTotalSalesForToysAndElectronics(purchasesData)
+    println("Total sales for Toys and Consumer Electronics:")
     println(toysAndElectronicsSales.collect().deep)
+  }
+
+  def computeTotalSalesForToysAndElectronics(rawData: RDD[String]): RDD[(String, BigDecimal)] = {
+    rawData
+      .map(line => line.split("\t"))
+      .filter(purchase => "Toys" == purchase(3) || "Consumer Electronics" == purchase(3))
+      .map(purchase => (purchase(3), scala.math.BigDecimal(purchase(4))))
+      .reduceByKey((a: BigDecimal, b: BigDecimal) => a + b)
   }
 }
 
