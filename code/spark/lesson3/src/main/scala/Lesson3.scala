cdb31319c6cbcd71e80ee0cfc2a683483da73730
akomar <ak.komar@gmail.com>
2014-05-16 05:07:05 +0200

    Added Spark solution to 2nd problem.

--- code/spark/lesson3/src/main/scala/Lesson3.scala
+++ code/spark/lesson3/src/main/scala/Lesson3.scala
@@ -10,11 +10,16 @@ object Lesson3 {
     val sc = new SparkContext("local", "Purchases", "/home/training/spark-0.9.1-bin-hadoop1/",
       List("target/scala-2.10/lesson-3_2.10-1.0.jar"))
 
-    val purchasesData = sc.textFile(purchasesFile).sample(false, 0.01, 1)
+    val purchasesData = sc.textFile(purchasesFile)//.sample(false, 0.01, 1)
 
     val toysAndElectronicsSales = computeTotalSalesForToysAndElectronics(purchasesData)
+    val highestSalesPerStore = computeHighestSaleForSelectedStores(purchasesData)
+
     println("Total sales for Toys and Consumer Electronics:")
     toysAndElectronicsSales.foreach(println)
+
+    println("Highest sales for selected stores:")
+    highestSalesPerStore.foreach(println)
   }
 
   def computeTotalSalesForToysAndElectronics(rawData: RDD[String]): Array[(String, BigDecimal)] = {
@@ -23,6 +28,15 @@ object Lesson3 {
       .filter(purchase => "Toys" == purchase(3) || "Consumer Electronics" == purchase(3))
       .map(purchase => (purchase(3), scala.math.BigDecimal(purchase(4))))
       .reduceByKey((a: BigDecimal, b: BigDecimal) => a + b)
+      .collect()
+  }
+
+  def computeHighestSaleForSelectedStores(rawData: RDD[String]): Array[(String, BigDecimal)] = {
+    rawData
+      .map(line => line.split("\t"))
+      .map(line => (line(2),BigDecimal(line(4))))
+      .reduceByKey((a: BigDecimal, b: BigDecimal) => if (a>b) a else b)
+      .filter((t: (String, BigDecimal)) => "Reno"==t._1 || "Toledo"==t._1 || "Chandler"==t._1)
       .collect()
   }
 }
